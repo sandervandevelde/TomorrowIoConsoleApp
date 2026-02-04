111e2236343744c6ebe0c66c700fe696c70634c0
sandervandevelde <svelde@gmail.com>
2026-02-04 21:58:35 +0100

    Key update

--- Program.cs
+++ Program.cs
@@ -8,19 +8,19 @@ namespace TomorrowIoConsoleApp
         {
             Console.WriteLine("Hello, Tomorrow IO!");
 
-            var key = "2C2qeEqsZDz33Tfm6mVb6w3rPA10qdDi";
+            var key = "Generated API KEY";
 
             // City name location=new york
-            //var options = new RestClientOptions($"https://api.tomorrow.io/v4/weather/forecast?location=new%20york&apikey={key}");
+            //var options = new RestClientOptions($"https://api.tomorrow.io/v4/weather/forecast?location=new%20york");
 
             // City name location=toronto
-            //var options = new RestClientOptions($"https://api.tomorrow.io/v4/weather/realtime?location=toronto&apikey={key}");
+            //var options = new RestClientOptions($"https://api.tomorrow.io/v4/weather/realtime?location=toronto");
 
             // Latitude and Longitude (Decimal degree) 
-            //var options = new RestClientOptions($"https://api.tomorrow.io/v4/weather/realtime?location=52.0,5.2&apikey={key}");
+            //var options = new RestClientOptions($"https://api.tomorrow.io/v4/weather/realtime?location=52.0,5.2");
 
-            //var options = new RestClientOptions($"https://api.tomorrow.io/v4/weather/history/recent?location=52.0,5.2&apikey={key}");
-            //var options = new RestClientOptions($"https://api.tomorrow.io/v4/weather/history/recent?location=52.0,5.2&apikey={key}&timesteps=1h&timesteps=1d&units=metric");
+            //var options = new RestClientOptions($"https://api.tomorrow.io/v4/weather/history/recent?location=52.0,5.2");
+            //var options = new RestClientOptions($"https://api.tomorrow.io/v4/weather/history/recent?location=52.0,5.2&timesteps=1h&timesteps=1d&units=metric");
             var options = new RestClientOptions($"https://api.tomorrow.io/v4/weather/history/recent?location=32.16134029123959,-49.228548806929815&timesteps=1h&timesteps=1d&units=metric");
 
             var client = new RestClient(options);
